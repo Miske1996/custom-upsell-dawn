d11eb75981438fe4fa3f09666de34b28e1be2c1a
unknown <miskelvilaly@gmail.com>
2023-10-19 10:22:25 +0000

    first commit

---
+++ assets/product-form.js
@@ -0,0 +1,137 @@
+if (!customElements.get('product-form')) {
+  customElements.define(
+    'product-form',
+    class ProductForm extends HTMLElement {
+      constructor() {
+        super();
+
+        this.form = this.querySelector('form');
+        this.form.querySelector('[name=id]').disabled = false;
+        this.form.addEventListener('submit', this.onSubmitHandler.bind(this));
+        this.cart = document.querySelector('cart-notification') || document.querySelector('cart-drawer');
+        this.submitButton = this.querySelector('[type="submit"]');
+
+        if (document.querySelector('cart-drawer')) this.submitButton.setAttribute('aria-haspopup', 'dialog');
+
+        this.hideErrors = this.dataset.hideErrors === 'true';
+      }
+
+      onSubmitHandler(evt) {
+        evt.preventDefault();
+        if (this.submitButton.getAttribute('aria-disabled') === 'true') return;
+
+        this.handleErrorMessage();
+
+        this.submitButton.setAttribute('aria-disabled', true);
+        this.submitButton.classList.add('loading');
+        this.querySelector('.loading__spinner').classList.remove('hidden');
+        //Here we are going to add the possibility to add one or multiple products
+        var productsToAdd = []
+        const checkedProducts = document.querySelectorAll(".bought-together-checkbox:checked");
+        const mainProduct = document.querySelector(".quantity__input");
+        this.productToAdd.push({
+          id: mainProduct.getAttribute("data-product-id"),
+          quantity : mainProduct.getAttribute("value")
+        })
+        if(checkedProducts.length>0) {
+          checkedProducts.forEach((product) => {
+            productsToAdd.push({
+              id: product.getAttribute("data-product-id"),
+              quantity: product.getAttribute('data-cart-quantity')
+            })
+          })
+        }
+        const items = productsToAdd.map((product) => ({
+          id:product.id ,
+          quantity : parseInt(product.quantity)
+        }))
+        const requesBody = {
+          items:items
+        }
+        ;
+        if (this.cart) {
+          requesBody.sections = this.cart.getSectionsToRender().map((section) => section.id);
+          requesBody.sections_url =  window.location.pathname;
+        }
+
+        fetch(`${window.Shopify.routes.root}cart/add.js`, {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body:JSON.stringify(requesBody)
+        })
+          .then((response) => response.json())
+          .then((response) => {
+            if (response.status) {
+              publish(PUB_SUB_EVENTS.cartError, {
+                source: 'product-form',
+                productVariantId: formData.get('id'),
+                errors: response.errors || response.description,
+                message: response.message,
+              });
+              this.handleErrorMessage(response.description);
+
+              const soldOutMessage = this.submitButton.querySelector('.sold-out-message');
+              if (!soldOutMessage) return;
+              this.submitButton.setAttribute('aria-disabled', true);
+              this.submitButton.querySelector('span').classList.add('hidden');
+              soldOutMessage.classList.remove('hidden');
+              this.error = true;
+              return;
+            } else if (!this.cart) {
+              window.location = window.routes.cart_url;
+              return;
+            }
+
+            if (!this.error)
+              publish(PUB_SUB_EVENTS.cartUpdate, {
+                source: 'product-form',
+                productVariantId: formData.get('id'),
+                cartData: response,
+              });
+            this.error = false;
+            const quickAddModal = this.closest('quick-add-modal');
+            if (quickAddModal) {
+              document.body.addEventListener(
+                'modalClosed',
+                () => {
+                  setTimeout(() => {
+                    this.cart.renderContents(response);
+                  });
+                },
+                { once: true }
+              );
+              quickAddModal.hide(true);
+            } else {
+              this.cart.renderContents(response);
+            }
+          })
+          .catch((e) => {
+            console.error(e);
+          })
+          .finally(() => {
+            this.submitButton.classList.remove('loading');
+            if (this.cart && this.cart.classList.contains('is-empty')) this.cart.classList.remove('is-empty');
+            if (!this.error) this.submitButton.removeAttribute('aria-disabled');
+            this.querySelector('.loading__spinner').classList.add('hidden');
+          });
+      }
+
+      handleErrorMessage(errorMessage = false) {
+        if (this.hideErrors) return;
+
+        this.errorMessageWrapper =
+          this.errorMessageWrapper || this.querySelector('.product-form__error-message-wrapper');
+        if (!this.errorMessageWrapper) return;
+        this.errorMessage = this.errorMessage || this.errorMessageWrapper.querySelector('.product-form__error-message');
+
+        this.errorMessageWrapper.toggleAttribute('hidden', !errorMessage);
+
+        if (errorMessage) {
+          this.errorMessage.textContent = errorMessage;
+        }
+      }
+    }
+  );
+}
